19c5a7627c077824bcaeeb3ba1dae28b17803f89
kleyla <kleyla01@live.com>
2020-06-16 17:52:25 -0400

    proteccion de controladores

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,6 +8,13 @@ use Spatie\Permission\Models\Role;
 
 class UserController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware(['permission:create user'], ['only' => ['create', 'store']]);
+        $this->middleware(['permission:read user'], ['only', 'index']);
+        $this->middleware(['permission:update user'], ['only' => ['edit', 'update']]);
+        $this->middleware(['permission:delete user'], ['only' => 'delete']);
+    }
     /**
      * Display a listing of the resource.
      *
